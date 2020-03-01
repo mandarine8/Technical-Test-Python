b7871f2b86a82d26f203d935ae3af918f71551cb
Amandine Car <caramandine@gmail.com>
2020-03-01 01:44:18 -0500

    Characters splitter exercise: Files, functions and tests

---
+++ char_splitter.py
@@ -0,0 +1,29 @@
+# Naive 10 Characters splitter exercise:
+
+# Cut every lign at 10 character maximum
+# The delimiter is the Linux one: \n
+# Every unsupported input return an empty string
+# String naive80CharSplitter(String input);
+
+def char_splitter(input_value):
+  if isinstance(input_value, str):
+    lines = input_value.splitlines(keepends=True)
+
+    splitted_lines = ""
+    for line in lines:
+      splitted_lines += splitter(line)
+
+    return splitted_lines
+  else:
+    return ""
+
+def splitter(line):
+  splitted_text = ""
+
+  while len(line) != 0:
+    splitted_text += line[:10]
+    if len(line) >= 10:
+      splitted_text += "\n"
+    line = line[10:]
+
+  return splitted_text
---
+++ char_splitter_test.py
@@ -0,0 +1,18 @@
+from char_splitter import char_splitter
+
+def test_one_word():
+  assert char_splitter("Test") == "Test"
+
+def test_this_is_a_test():
+  assert char_splitter("This is a test") == "This is a \ntest"
+
+def test_multiline():
+  assert char_splitter("This is a test\nHow are things going?") == "This is a \ntest\nHow are th\nings going\n?"
+
+def test_inline():
+  assert char_splitter("\n") == "\n"
+
+def test_invalid_input():
+  assert char_splitter(3) == ""
+  assert char_splitter(["This is", "a test"]) == ""
+  assert char_splitter(True) == ""
